ea9218323a9c06e1ee94a4bbe225d013316bad9e
Andrei Tatar <andrei.ttr@gmail.com>
2023-04-02 23:54:22 +0200

    Add unittest for btrfs.Vol

--- test/test_btrfs.py
+++ test/test_btrfs.py
@@ -10,6 +10,36 @@ import unittest
 from btrsync import btrfs
 
 
+class TestVol(unittest.TestCase):
+	VKEYS = {btrfs.Vol._P, btrfs.Vol._S}
+
+	def test_def(self):
+		v = btrfs.Vol()
+		for k in v:
+			if k not in self.VKEYS:
+				self.fail(f'Unexpected key {k}')
+		self.assertIsNone(v.parent)
+		self.assertEqual(v.succs, [])
+		v.parent = 42
+		v.succs.extend((1, 2, 3))
+		self.assertEqual(v.parent, 42)
+		self.assertEqual(v.succs, [1, 2, 3])
+
+	def test_example(self):
+		d = {'path': 'my/path', 'uuid': 1337}
+		v = btrfs.Vol(d, parent=42, succs=[4, 5, 6])
+		for k in d:
+			self.assertEqual(v[k], d[k])
+		for k in v:
+			if k not in self.VKEYS:
+				if k in d:
+					self.assertEqual(v[k], d[k])
+				else:
+					self.fail(f'Unexpected key {k}')
+		self.assertEqual(v.parent, 42)
+		self.assertEqual(v.succs, [4, 5, 6])
+
+
 class TestAbsPaths(unittest.TestCase):
 	def test_bad(self):
 		it = btrfs.abspaths([], 'bad/relative/rootpath')
@@ -24,7 +54,6 @@ class TestAbsPaths(unittest.TestCase):
 		exvols = ({'path': p} for p in exp)
 		for t, e in zip(btrfs.abspaths(invols, '<FS_TREE>/testvol'), exvols):
 			self.assertEqual(t, e)
-
 
 
 class TestRelPaths(unittest.TestCase):
